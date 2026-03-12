3af971482b773f223d736da5effad02902801d56
Nevesh Divya <neveshdivya@github.com>
2026-03-12 16:14:26 +0530

    Add existing project files to Git

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        // "service_axbtt7a",  // Replace with your EmailJS Service ID
-        // "template_1ziboq3",  // Replace with your EmailJS Template ID
-        // form.current,
-        // "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+         "service_llqs8af",  // Replace with your EmailJS Service ID
+         "template_acdh22o",  // Replace with your EmailJS Template ID
+         form.current,
+         "p2iKbRlvQaKp3xB3v"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
